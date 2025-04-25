6da4a422112a7e66ed762490baa522a37237ca58
OlufunbiIK <olufunbiibrahim@gmail.com>
2025-04-25 13:29:53 +0100

    feat:Implement PostgreSQL Integration & Migration System with TypeORM #1

---
+++ src/migrations/1745583143074-InitialMigration.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class InitialMigration1745583143074 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
